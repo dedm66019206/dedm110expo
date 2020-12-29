54d0f57e6e96b576af9a1ce18d70831c938e91e1
dedm66019206 <74500920+dedm66019206@users.noreply.github.com>
2020-12-30 04:23:07 +0800

    Add files via upload

---
+++ index/script/modal.js
@@ -0,0 +1,201 @@
+function modal(){
+  // Get the modal
+  var body = document.getElementsByTagName("body")[0]; 
+  var html = document.getElementsByTagName("html")[0]; 
+  var modal1 = document.getElementById("myModal_1");
+  var modal2 = document.getElementById("myModal_2");
+  var modal3 = document.getElementById("myModal_3");
+  var modal4 = document.getElementById("myModal_4");
+  var modal5 = document.getElementById("myModal_5");
+  var modal6 = document.getElementById("myModal_6");
+  var modal7 = document.getElementById("myModal_7");
+  var modal8 = document.getElementById("myModal_8");
+  var modal9 = document.getElementById("myModal_9");
+  var modal10 = document.getElementById("myModal_10");
+  var modal11 = document.getElementById("myModal_11");
+  var modal12 = document.getElementById("myModal_12");
+  // Get the button that opens the modal
+  var oBox = document.getElementById('changeBox');
+  var team1 = document.getElementById("team1");
+  var team2 = document.getElementById("team2");
+  var team3 = document.getElementById("team3");
+  var team4 = document.getElementById("team4");
+  var team5 = document.getElementById("team5");
+  var team6 = document.getElementById("team6");
+  var team7 = document.getElementById("team7");
+  var team8 = document.getElementById("team8");
+  var team9 = document.getElementById("team9");
+  var team10 = document.getElementById("team10");
+  var team11 = document.getElementById("team11");
+  var team12 = document.getElementById("team12");
+  var oBigView = oBox.getElementsByClassName('bigView')[0];
+  var oBack = oBigView.getElementsByClassName('back')[0];
+  var oNext = oBigView.getElementsByClassName('next')[0];
+
+
+  // Get the <span> element that closes the modal
+  var close1 = document.getElementsByClassName("close1")[0];
+  var close2 = document.getElementsByClassName("close2")[0];
+  var close3 = document.getElementsByClassName("close3")[0];
+  var close4 = document.getElementsByClassName("close4")[0];
+  var close5 = document.getElementsByClassName("close5")[0];
+  var close6 = document.getElementsByClassName("close6")[0];
+  var close7 = document.getElementsByClassName("close7")[0];
+  var close8 = document.getElementsByClassName("close8")[0];
+  var close9 = document.getElementsByClassName("close9")[0];
+  var close10 = document.getElementsByClassName("close10")[0];
+  var close11 = document.getElementsByClassName("close11")[0];
+  var close12 = document.getElementsByClassName("close12")[0];
+
+  team1.onclick = function() {
+    modal1.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close1.onclick = function() {
+    modal1.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+
+  team2.onclick = function() {
+    modal2.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close2.onclick = function() {
+    modal2.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+
+  team3.onclick = function() {
+    modal3.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close3.onclick = function() {
+    modal3.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+  team4.onclick = function() {
+    modal4.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close4.onclick = function() {
+    modal4.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+
+  team5.onclick = function() {
+    modal5.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close5.onclick = function() {
+    modal5.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+  team6.onclick = function() {
+    modal6.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close6.onclick = function() {
+    modal6.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+
+  team7.onclick = function() {
+    modal7.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close7.onclick = function() {
+    modal7.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+  team8.onclick = function() {
+    modal8.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close8.onclick = function() {
+    modal8.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+  team9.onclick = function() {
+    modal9.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close9.onclick = function() {
+    modal9.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+  team10.onclick = function() {
+    modal10.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close10.onclick = function() {
+    modal10.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+  team11.onclick = function() {
+    modal11.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close11.onclick = function() {
+    modal11.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+  team12.onclick = function() {
+    modal12.style.display = "block";
+    oBack.style.display = "none";
+    oNext.style.display =  "none";
+    html.style.overflow = "hidden";
+  }
+  close12.onclick = function() {
+    modal12.style.display = "none";
+    oBack.style.display = "block";
+    oNext.style.display = "block";
+    html.style.overflow = "auto";
+  }
+
+
+
+}
